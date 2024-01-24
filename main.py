1ab0f388c32c3d9cc6301210c73e5d1fda3ea47f
PogiAbel <pogonyiabel@gmail.com>
2024-01-24 16:14:15 +0100

    cleanup, line art

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
+from pygame import Vector2
 import math
 
 class Circle():
-    def __init__(self, radius: int, point: pygame.Vector2,color, width = 0) -> None:
+    def __init__(self, radius: int, point: Vector2,color, width = 0) -> None:
         self.radius = radius
         self.point = point.copy()
         self.color = color
@@ -10,19 +11,29 @@ class Circle():
 
     def draw(self):
         pygame.draw.circle(screen, self.color, self.point, self.radius, self.width)
-
+        
+    def getPointOnCircle(self, angle: float) -> Vector2:
+        return Vector2(self.radius * math.cos(angle), self.radius * math.sin(angle)) + self.point
+    
+    def pointToCircle(self, point: Vector2) -> Vector2:
+        return (point - self.point).normalize() * (self.radius * 0.95) + self.point
+    
 class Ball (Circle):
-    def __init__(self, radius: int, point: pygame.Vector2, color,speed,mass) -> None:
+    def __init__(self, radius: int, point: Vector2, color,speed,mass, velocity: Vector2 = Vector2(0,0),outline_width=0, outline_color="white") -> None:
         super().__init__(radius, point, color, 0)
         self.radius = radius
         self.point = point.copy()
         self.color = color
         self.sepeed = speed
         self.mass = mass
-        self.velocity = pygame.Vector2(0,0)
+        self.velocity = velocity
+        self.outline_width = outline_width
+        self.outline_color = outline_color
         self.original = (radius, point.copy(), color, mass, self.velocity.copy())
 
     def draw(self):
+        if self.outline_width != 0:
+            pygame.draw.circle(screen, self.outline_color, self.point, self.radius + self.outline_width, self.outline_width)
         pygame.draw.circle(screen, self.color, self.point, self.radius, self.width)
 
     def reset(self):
@@ -74,6 +85,11 @@ def OutsideCircle(outer_circle: Circle, inner_circle: Circle):
         return False
     return True
 
+def Line(point1: Vector2, point2: Vector2, color, width = 1):
+    pygame.draw.line(screen, color, point1, point2, width)
+
+def SimpleCollision(ball: Ball, circle: Circle):
+    ball.velocity = ball.velocity.reflect(circle.point - ball.point)
 
 # pygame setup
 pygame.init()
@@ -84,13 +100,12 @@ running = True
 dt = 0
 
 # Physics
-start_velocity = pygame.Vector2(50,-10)
-gravitation = pygame.Vector2(0, 1)
-velocity = start_velocity.copy()
-dump = 0.5
+start_velocity = Vector2(50,-10)
+gravitation = Vector2(0, 2)
+dump = 0.8
 speed = 10
 
-screen_middle = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+screen_middle = Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 current_fps = 0
 
@@ -98,25 +113,32 @@ screen.fill("black")
 
 hit_sound = pygame.mixer.Sound("sounds/hit.wav")
 
-offset = pygame.Vector2(0,0)
+offset = Vector2(0,0)
 
-center_circle = Circle(200, screen_middle,"blue", 3)
+center_circle = Circle(200, screen_middle,"blue", 8)
 
-player_circle = Ball(10, screen_middle - offset, "red", 10, 10)
+player_circle = Ball(10, screen_middle - offset, "red", 10, 5,start_velocity.copy(),outline_width=0)
 
 def refresh():
     screen.fill("black")
-    center_circle.draw()
-    player_circle.draw()
+    
     for x in buttons: 
         x.draw()
+    for x in lines:
+        Line(x[0], x[1], "white", 2)
+        
+    center_circle.draw()
+    player_circle.draw()
 
 # Buttons
 buttons: list[Button] = []
 reset_button = Button(10, 10, 100, 50, "green", "Reset", "white", font)
-buttons.append(reset_button)
+#buttons.append(reset_button)
 
+# Lines
+lines : list[Vector2] = []
 
+start = False
 
 while running:
 
@@ -132,27 +154,44 @@ while running:
                 if event.button == 1:
                     if reset_button.is_clicked(event.pos):
                         player_circle.reset()
-                        velocity = start_velocity.copy()
+                        lines.clear()
+            case pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    start = not start
+                if event.key == pygame.K_r:
+                    player_circle.reset()
+                    lines.clear()
+            case pygame.WINDOWMOVED:
+                screen_middle = Vector2(screen.get_width() / 2, screen.get_height() / 2)
+                continue
 
+    if start:
+        
+        player_circle.velocity += gravitation * player_circle.mass
 
-    if OutsideCircle(center_circle, player_circle) == True:
-        new_point = (center_circle.point - player_circle.point).normalize() * 2
-        player_circle.point += new_point
-        #player_circle.velocity = pygame.Vector2(center_circle.point - player_circle.point).normalize() * velocity.length()
-        player_circle.velocity = player_circle.velocity.reflect(pygame.Vector2(center_circle.point - player_circle.point).normalize())
-        if velocity.length() > 1:
-            pygame.mixer.Sound.play(hit_sound)
-        else:
-            player_circle.velocity = pygame.Vector2(0,0)
+        if OutsideCircle(center_circle, player_circle) == True:
+            # move inside to the circle
+            new_point = (center_circle.point - player_circle.point).normalize() * 2
+            player_circle.point += new_point
+
+            # calculate new velocity
+            SimpleCollision(player_circle, center_circle)
             
-    # Velocity calculation
-    player_circle.velocity += gravitation * player_circle.mass * speed
-    player_circle.point += player_circle.velocity * dt
+            lines.append((center_circle.pointToCircle(player_circle.point), player_circle.point))
+            
+            if player_circle.velocity.length() > 16:
+                pygame.mixer.Sound.play(hit_sound)
+            else:
+                player_circle.velocity = Vector2(0,0)
+                player_circle.mass = 0
+                
+        # Velocity calculation
+        player_circle.point += player_circle.velocity * dt
 
-    
-    # check if player is out of bounds
-    if player_circle.radius > center_circle.radius:
-        running = False
+        
+        # check if player is out of bounds
+        if player_circle.radius > center_circle.radius:
+            running = False
 
     refresh()
     pygame.display.flip()
